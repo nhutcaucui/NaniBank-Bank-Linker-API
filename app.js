20f6268d6b7bebaf0a1779e54a810503983143d3
Nhựt <nhutcaucui@gmail.com>
2020-05-21 19:03:25 +0700

    fix userRouter reference to a non-existent file

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var timeValidateMiddleware = require('./middleware/timeValidate');
 var transactionRouter = require('./routes/transaction');
-var usersRouter = require('./routes/user/users');
+var usersRouter = require('./routes/user/user');
 
 var app = express();
 

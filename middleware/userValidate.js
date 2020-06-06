2ad7dd91d0f6b1c7858a586166149c84a826e4c0
nvnamsss <nvnam.c@gmail.com>
2020-06-06 17:08:59 +0700

    implemented user validate middleware

--- middleware/userValidate.js
+++ middleware/userValidate.js
@@ -1,5 +1,31 @@
+const jwt = require("jsonwebtoken");
+const config = require('config');
+const secret_key = config.get('secret-key');
+const moment = require('moment');
+
 function userValidate(req, res, next) {
-    next();
+    let token = req.headers['access-token'];
+    if (token == undefined) {
+        res.status(200).send({
+            "Status": false,
+            "Message": "Authorization needed"
+        });
+
+        return;
+    }
+
+    jwt.verify(token, secret_key, function (err, decoded) {
+        if (err) {
+            res.status(200).send({
+                "Status": false,
+                "Message": err.message
+            });
+
+            return;
+        }
+
+        next();
+    });
 }
 
 module.exports = userValidate;

4aba2d40f30f49700310611e2cb1c19471d432db
nvnamsss <nvnam.c@gmail.com>
2020-06-08 22:13:45 +0700

    transfer now will be a method of debit, methods that related to transaction will create perspective history

--- model/customers/debit_account.js
+++ model/customers/debit_account.js
@@ -2,16 +2,17 @@ const db = require('../db');
 const moment = require('moment');
 const tablename = "debit_account";
 const balance = "balance";
+const history = require('../transaction_history');
 
 async function getByOwner(owner) {
-    let result = await db.loaddb(`SELECT * FROM ${tablename} WHERE owner=${owner}`);
+    let result = await db.query(`SELECT * FROM ${tablename} WHERE owner=${owner}`);
     if (result.length == 0) return new Error("This customer does not have any debit account");
 
     return result[0];
 }
 
 async function getById(id) {
-    let result = await db.loaddb(`SELECT * FROM ${tablename} WHERE id=${id}`);
+    let result = await db.query(`SELECT * FROM ${tablename} WHERE id=${id}`);
     if (result.length == 0) return new Error("Debit account was not found");
 
     return result[0];
@@ -23,7 +24,7 @@ async function getById(id) {
  */
 async function create(owner) {
     let id = 9704366600000000 + owner;
-    let result = await db.loaddb(`SELECT * FROM ${tablename} WHERE id=${id}`);
+    let result = await db.query(`SELECT * FROM ${tablename} WHERE id=${id}`);
     if (result.length > 0) return new Error("User already have the debit account");
 
     let issue_date = moment().add(3, 'years').unix();
@@ -34,8 +35,11 @@ async function create(owner) {
         balance : 0,
         issue_date : issue_date,
     }
+    
+    result = await db.addtb(tablename, entity);
+    if (result instanceof Error) return result;
 
-    return await db.addtb(tablename, entity);
+    return entity;
 }
 
 /**
@@ -43,7 +47,7 @@ async function create(owner) {
  * @param {*} id id of the debit account
  * @param {*} amount amount of money need to be charged, amount must not negative
  */
-async function charge(id, amount) {
+async function charge(id, amount, message = "Charge " + amount  + " to debit account") {
     if (amount <= 0) return Error("Amount cannot be negative");
 
     let result = await getById(id);
@@ -60,6 +64,7 @@ async function charge(id, amount) {
         balance: account.balance,
     };
 
+    history.create(id, id, amount, 0, message);
     return await db.updatetb(tablename, conditionEntity, valueEntity);
 }
 /**
@@ -67,7 +72,7 @@ async function charge(id, amount) {
  * @param {*} id id of the debit account
  * @param {*} amount amount of money need to be drawed, amount must not negative
  */
-async function draw(id, amount) {
+async function draw(id, amount, message = "Draw " + amount + " from debit account") {
     if (amount <= 0) return Error("Amount cannot be negative");
     let result = await getById(id);
 
@@ -88,7 +93,45 @@ async function draw(id, amount) {
         balance: account.balance,
     }
 
+    history.create(id, id, -amount, 1, message);
     return await db.updatetb(tablename, conditionEntity, valueEntity);
+}
+
+/**
+ * Transfer money from an account to another
+ * @param {*} fromId 
+ * @param {*} toId 
+ * @param {*} amount 
+ * @param {*} message 
+ */
+async function transfer(fromId, toId, amount, message) {
+    if (amount <= 0) return Error("Amount cannot be negative");
+    let result = await getById(id);
+
+    if (result instanceof Error) return result;
+
+    let fromAccount = await getById(fromId);
+    if (fromAccount instanceof Error) {
+        return fromAccount;
+    } 
+
+    let toAccount = await getById(toId); 
+    if (toAccount instanceof Error) {
+        return toAccount;
+    }
+
+    if (fromAccount.balance < amount) {
+        return new Error("Balance is not enough");
+    }
+
+    fromAccount.balance -= amount;
+    toAccount.balance += amount;
+
+    db.updatetb(tablename, {id : fromAccount.id}, {balance: fromAccount.balance});
+    db.updatetb(tablename, {id : toAccount.id}, {balance: toAccount.balance});
+    history.create(fromAccount.id, toAccount.id, amount, 3, message);
+
+    return {fromAccount, toAccount};
 }
 
 module.exports = {
@@ -97,4 +140,5 @@ module.exports = {
     create,
     draw,
     charge,
+    transfer
 }

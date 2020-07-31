8dad160c7d1fb52858f227fcb4c0ca452ca9e57f
Nhựt <nhutcaucui@gmail.com>
2020-07-31 22:51:59 +0700

    add a route for verify otp before outside transaction

--- router/otp.js
+++ router/otp.js
@@ -6,6 +6,10 @@ const mail_host = config.get('mail-host');
 const userMiddleware = require('../middleware/userValidate');
 const createOtp = require('../middleware/otpValidate').createOtp;
 const customer = require('../model/customers/customer');
+const hotp = require('otplib').hotp;
+const authenticator = require('otplib').authenticator;
+const moment = require('moment');
+
 router.get('/create', [userMiddleware], async function(req, res) {
     let access_token = req.headers["access-token"];
     let pack = createOtp(access_token);
@@ -92,6 +96,56 @@ router.get('/s-create', async function(req, res) {
 
 router.get('/ping', function(req, res) {
     res.status(200).send("ping");
+})
+
+router.post('/check', [userMiddleware], function (req, res, next) {
+    let access_token = req.headers["access-token"];
+    let key = req.headers["key"];
+    let otp = req.headers["otp"];
+    
+    if (access_token == undefined) {
+        res.status(200).send({
+            "Status" : false,
+            "Message" : "access-token header is missing"
+        });
+    }
+    if (otp == undefined) {
+        res.status(200).send({
+            "Status" : false,
+            "Message" : "otp header is missing"
+        });
+        return;
+    }
+
+    if (key == undefined) {
+        res.status(200).send({
+            "Status" : false,
+            "Message" : "key header is missing"
+        });
+
+        return;
+    }
+    key = Number(key);
+    if (moment().unix() - key > 60) {
+        res.status(200).send({
+            "Status" : false,
+            "Message" : "OTP is expired"
+        });
+        return;
+    }
+
+    if (!hotp.check(otp, access_token, key)) {
+        res.status(200).send({
+            "Status" : false,
+            "Message" : "Invalid OTP"
+        });
+        return;
+    }
+
+    res.status(200).send({
+        "Status" : true,
+        "Message" : "Invalid OTP"
+    });
 })
 
 module.exports = router;

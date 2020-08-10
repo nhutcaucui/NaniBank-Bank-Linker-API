5f88e9b16d4659c469164ab892d7ac06bff801f5
Nhựt <nhutcaucui@gmail.com>
2020-08-10 23:50:57 +0700

    cors middleware

--- app.js
+++ app.js
@@ -7,10 +7,6 @@ var logger = require('morgan');
 //var cors = require('cors');
 
 //cors for browser can call the API
-const cors = {
-  origin: ["http://localhost:8080","http://192.168.1.6:8080","www.lam.com","www.nguyen.com","www.nanibank.com"],
-  default: "http://localhost:8080/"
-}
 
 var timeValidateMiddleware = require('./middleware/timeValidate');
 var transactionRouter = require('./router/transaction');
@@ -20,33 +16,21 @@ var debtRouter = require('./router/debt');
 var debitRouter= require('./router/debit_account');
 var savingRouter = require('./router/saving_account');
 var otpRouter = require('./router/otp');
+var corsMiddleware = require('./middleware/cors');
 // var io = require('./realtime/io');
 
 require('dotenv').config()
 
 var app = express();
-// app.use(cors());
-app.all('*', function(req, res, next) {
-  let origin = req.header('origin');
-  if (origin == undefined) {
-    res.status(200).send( {
-      "Status" : false,
-      "Message": "origin header is missing"
-    });
-    return;
-  }
-  origin = cors.origin.indexOf(origin.toLowerCase()) > -1 ? req.headers.origin : cors.default;
-  res.header("Access-Control-Allow-Origin", origin);
-  res.header("Access-Control-Allow-Headers", '*');
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE, PATCH");
-  next();
-});
+
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(corsMiddleware);
 app.use(timeValidateMiddleware);
 app.use('/transaction', transactionRouter);
 app.use('/users', usersRouter);
